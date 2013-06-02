ab3830c50e8a79971b13f11770dae214b1ee8dad
Kevin Young <kevin_young@intuit.com>
2013-06-02 16:53:18 -0700

    updated attributes

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,15 +1,11 @@
 include_recipe 'user'
 include_recipe 'user::data_bag'
 
-#node.set['user']['user_array_node_attr'] = "app_env_settings/users"
-node.set['users'] = node['environment_users']['users']
+node.set['user']['user_array_node_attr'] = node['environment_users']['users']
 
-#remove_accounts = node['app_env_settings']['users_to_remove']
-remove_accounts = node['environment_users']['users_to_remove']
-remove_accounts.each do |username|
+node['environment_users']['users_to_remove'].each do |username|
   user_account username do
     action :remove
     ignore_failure true
   end
 end
-
